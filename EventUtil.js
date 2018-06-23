8b038a8eb66dc444e0b8d0a47fede55865f3a486
zrd <17805009970@163.com>
2018-06-23 20:51:08 +0800

    Update EventUtil.js

--- EventUtil.js
+++ EventUtil.js
@@ -1,1 +1,48 @@
+var EventUtil = {
+  
+    addHandler: function(el, type, handler) {
+        if (el.addEventListener) {
+            el.addEventListener(type, handler, false)
+        } else if (el.attachEvent) {
+            el.attachEvent('on' + type, handler)
+        } else {
+            el['on' + type] = handler
+        }
+    },
+  
+    removeHandler: function(el, type, handler) {
+        if (el.removeEventListener) {
+            el.removeEventListener(type, handler, false)
+        } else if (el.detachEvent) {
+            el.detachEvent('on' + type, handler)
+        } else {
+            el['on' + type] = null
+        }
+    },
+  
+    getEvent: function(event) {
+        return event || window.event
+    },
+  
+    getTarget: function(event) {
+        return event.target || event.srcElement
+    },
+  
+    preventDefault: function(event) {
+        if (event.preventDefault) {
+            event.preventDefault()
+        } else {
+            event.returnValue = false
+        }
+    },
+  
+    stopPropagation: function(event) {
+        if (event.stopPropagation) {
+            event.stopPropagation()
+        } else {
+            event.cancelBubble = true
+        }
+    }
+  
+}
 
