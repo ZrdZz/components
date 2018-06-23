9f82999b2514a989e7ac422af20c60da618788d1
zrd <17805009970@163.com>
2018-06-23 21:19:15 +0800

    Update Drag.js

--- Drag.js
+++ Drag.js
@@ -2,10 +2,10 @@
   function Drag(ele){
     this.ele = typeof ele === Object ? ele : document.getElementById(ele);
 
-    //鼠标的初始位置
+    // 鼠标的初始位置
     this.startX = 0;
     this.startY = 0;
-    //物体的初始位置
+    // 物体的初始位置
     this.targetX = 0;
     this.targetY = 0;
 
@@ -19,20 +19,20 @@
       this.setDrag();
     },
 
-    //得到对象的位置
+    // 得到对象的位置
     getPos: function(){
       var pos = {x: 0, y: 0},
           transformValue = this.getStyle('transform');
 
       if('transform' in document.documentElement.style){
-        //transform初始为none
+        // transform初始为none
         if(transformValue === 'none'){
           this.ele.style.transform = 'translate(0, 0)';
         }else{
-          //负号用于匹配当物体有一部分在屏幕外边时, 数值为负; ？指匹配0或一个
+          // 负号用于匹配当物体有一部分在屏幕外边时, 数值为负; ？指匹配0或一个
           var temp = transformValue.match(/-?\d+/g);
         
-          //transformValue是一个矩阵, translate的x和y为第5和第6个值
+          // transformValue是一个矩阵, translate的x和y为第5和第6个值
           pos = {
             x: parseInt(temp[4].trim()),
             y: parseInt(temp[5].trim())
@@ -54,7 +54,7 @@
       return document.defaultView.getComputedStyle ? document.defaultView.getComputedStyle(this.ele)[property] : this.ele.currentStyle[property]
     },
 
-    //设置对象的位置
+    // 设置对象的位置
     setPos: function(pos){
         if('transform' in document.documentElement.style){
           this.ele.style.transform = 'translate(' + pos.x + 'px,' + pos.y + 'px)';
@@ -64,7 +64,7 @@
         }
     },
 
-    //用于绑定事件
+    // 用于绑定事件
     setDrag: function(){
       var that = this;
       this.ele.addEventListener('mousedown', start, false);
@@ -101,3 +101,81 @@
   }
   window.Drag = Drag;
 })()
+
+
+// 高程上的
+var DragDrop = (function() {
+    let dragging = null
+    let diffX = 0
+    let diffY = 0
+    
+    function handleEvent(event) {
+        event = EventUtil.getEvent(event)
+        let target = EventUtil.getTarget(event)
+        
+        switch (target.type) {
+            case 'mousedown':
+                if (target.className.indexOf('draggable') > -1) {
+                    dragging = target
+                    diffX = event.clientX - target.offsetLeft
+                    diffY = event.clientY - target.offsetTop
+                }
+                break
+            case 'mousemove':
+                if (dragging !== null) {
+                    if (dragging.style.transform === 'static') {
+                        dragging.style.transform = 'absolute'
+                    }
+                    dragging.style.left = (event.clientX - diffX) + 'px'
+                    dragging.style.top = (event.clientY - diffY) + 'px'               
+                }
+                break
+            case 'mouseup':
+                dragging = null
+                break
+        }
+    }
+  
+    return {
+        enable: function() {
+            EventUtil.addHandler(document, 'mousedown', handleEvent)  
+            EventUtil.addHandler(document, 'mousemove', handleEvent) 
+            EventUtil.addHandler(document, 'mouseup', handleEvent) 
+        },
+        disable: function() {
+            EventUtil.removeHandler(document, 'mousedown', handleEvent)
+            EventUtil.removeHandler(document, 'mousemove', handleEvent)
+            EventUtil.removeHandler(document, 'mouseup', handleEvent)
+        }
+    }
+})()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
